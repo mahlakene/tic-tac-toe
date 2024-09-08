7fd73d9761e450c3e62ac27ae35bed232fd9518b
mikeso <mikeso@ttu.ee>
2024-09-08 16:01:10 +0300

    board squares

--- scripts.js
+++ scripts.js
@@ -3,4 +3,10 @@ function setActive(buttonId) {
     const buttons = document.querySelectorAll('.option-button');
     buttons.forEach(button => button.classList.remove('active'));
     document.getElementById(buttonId).classList.add('active');
+    if (buttonId === 'single-player') {
+        sessionStorage.setItem('gameMode', 'single');
+    } else {
+        sessionStorage.setItem('gameMode', 'two');
+    }
+    /*window.location.href = '../board/board.html';*/
 }
